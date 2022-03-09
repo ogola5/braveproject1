4478c73500e3d2b4042fcd7e25497c5d3cf8f8da
ogola5 <68680021+ogola5@users.noreply.github.com>
2022-03-10 02:46:13 +0300

    Add files via upload

---
+++ register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src ="js/query.min.js></script>
+    <script> src="js/popper.min.js"></script>
+
+</head>
+<body  class="bg-primary">
+<div class="container bg-white">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-5 bg register-image">
+                    image here
+                </div>
+                <div class="col-lg-7">
+                    form here
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+</body>
+</html>
